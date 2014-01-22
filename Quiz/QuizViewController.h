bdc07fb5ceca6c76e775259088b51117d9a6ad98
Michael Toth <michael@virtualpianist.com>
2014-01-22 11:35:45 -0500

    Initial Commit

---
+++ Quiz/QuizViewController.h
@@ -0,0 +1,13 @@
+//
+//  QuizViewController.h
+//  Quiz
+//
+//  Created by Michael Toth on 1/22/14.
+//  Copyright (c) 2014 Michael Toth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QuizViewController : UIViewController
+
+@end
---
+++ Quiz/QuizViewController.m
@@ -0,0 +1,29 @@
+//
+//  QuizViewController.m
+//  Quiz
+//
+//  Created by Michael Toth on 1/22/14.
+//  Copyright (c) 2014 Michael Toth. All rights reserved.
+//
+
+#import "QuizViewController.h"
+
+@interface QuizViewController ()
+
+@end
+
+@implementation QuizViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
